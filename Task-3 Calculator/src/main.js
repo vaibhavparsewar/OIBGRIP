3c0c339677c4325441df035864ffc3a8333535c9
vaibhav parsewar <82768659+vaibhavparsewar@users.noreply.github.com>
2022-01-17 01:15:45 +0530

    Add files via upload

---
+++ Task-3 Calculator/src/main.js
@@ -0,0 +1,43 @@
+// var invalid = document.getElementById("screen").value;
+let invalid = document.querySelector("#screen");
+function send(){
+    if (invalid.value == "0/0"){
+        alert("invalid input")
+        console.log(invalid.value);
+    }
+// }
+// if (invalid == NaN){
+//    alert("invalid input");
+// }
+
+// if (invalid === "NaN"){
+//     alert("invalid");
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function () {
+//     navList.classList.toggle('show')
+// }
+
+// vaibutton.addEventListener('click', toggleButton)
+
+// alert("hi vaibhav")
